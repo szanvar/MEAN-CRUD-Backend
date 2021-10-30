335b712dd77ce9133d689baf15068a565dc691cc
seemazanvar <srzanvar_dcd@jspm.edu.in>
2021-10-30 18:55:15 +0530

    back-end api built with Node.js, Express, MongoDB

--- connection.js
+++ connection.js
@@ -2,34 +2,34 @@ const mongoose = require("mongoose");
 
 //MongoDB Atlas
 
-// mongoose.connect("mongodb+srv://seemaz:seema123@cluster0.iq5wb.mongodb.net/MEAN?retryWrites=true&w=majority",
-// {useNewUrlParser: true,useUnifiedTopology: true}, function(err)
-// {
-//     if(err)
-//     {
-//         console.log("Unable to Connect");
-//     }
-//     else
-//     {
-//         console.log("Connection To MongoDB Atlas Successfully...");
-//     }
-// });
+mongoose.connect("mongodb+srv://seemaz:seema123@cluster0.iq5wb.mongodb.net/MEAN?retryWrites=true&w=majority",
+{useNewUrlParser: true,useUnifiedTopology: true}, function(err)
+{
+    if(err)
+    {
+        console.log("Unable to Connect");
+    }
+    else
+    {
+        console.log("Connection To MongoDB Atlas Successfully...");
+    }
+});
 
 
 
 // MongoDB Compass
 
-mongoose.connect("mongodb://localhost:27017/MEANDB", function(err)
-{
-    if(err)
-    {
-        console.log("Unable to connect");
-    }
-    else
-    {
-        console.log("Connection with MongoDB Compass is successfully");
-    }
-});
+// mongoose.connect("mongodb://localhost:27017/MEANDB", function(err)
+// {
+//     if(err)
+//     {
+//         console.log("Unable to connect");
+//     }
+//     else
+//     {
+//         console.log("Connection with MongoDB Compass is successfully");
+//     }
+// });
 
 
 
